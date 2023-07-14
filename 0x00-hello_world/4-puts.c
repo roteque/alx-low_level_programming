7e4274ffc652f2df38952dd7e547a03441170f7f
roteque <olusomogoye@gmail.com>
2023-07-13 17:07:27 -0700

    Hello ,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include < stdio.hi >
+/*
+ * main - prints a line  with puts command
+ * return:0 (succesfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
