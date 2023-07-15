edb875f2c0b6e91f0eb12fcfa1361fa4efbadd8e
roteque <olusomogoye@gmail.com>
2023-07-15 05:50:09 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
+/**
+ * main - A program to test numbers
+ * return 0 (success)
+ */
 int main(void)
 {
 	int n;
