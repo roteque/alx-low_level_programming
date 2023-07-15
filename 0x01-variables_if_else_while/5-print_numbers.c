0d4fc7ed39727df7e1a672e8be6207262dcf6909
roteque <olusomogoye@gmail.com>
2023-07-15 10:43:56 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - A program that prints all lower case alphabet except q and e
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	return (0);
+}
