42662d688017eac8d3ab961ed54ffeece9198ad4
roteque <olusomogoye@gmail.com>
2023-07-18 12:08:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints _putchar using putchar function
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char i = "putchar";
+
+	putchar(i);
+return (0);
+}
