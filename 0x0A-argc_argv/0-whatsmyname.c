c0ddd8e7242e1fdb9dd037b9a88b17922f3f8836
roteque <olusomogoye@gmail.com>
2023-08-05 10:38:51 -0700

    Task 0 corrected

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - this program writes the name of itself
- * @./mynameis ./mynameis
- * Return: 0 (Success)
- */
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
