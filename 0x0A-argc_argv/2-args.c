819cf8c1e94fc94807a64208a311b732cf7deb1e
roteque <olusomogoye@gmail.com>
2023-08-05 15:03:11 -0700

    Task 2 done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,12 +13,10 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	if (argc > 0)
-	{
-		for (i = 0; i < argc; i++)
+	
+	for (i = 0; i < argc; i++)
 		{
 			printf("%s \n", argv[i]);
 		}
-	}
 	return 0;
 }
