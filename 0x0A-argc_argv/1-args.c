ba50b886ba864308c4b6d09f5efb41751445cffb
roteque <olusomogoye@gmail.com>
2023-08-05 10:10:50 -0700

    readme file and task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints the number of arguments passed into it
+ * the program prints a number followed by a new line.
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return 0;
+}
