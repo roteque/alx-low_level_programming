732ba70ad8d6d42917b4cda5242e2728a1e78625
roteque <olusomogoye@gmail.com>
2023-08-05 10:50:50 -0700

    Task 1 corrected

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char __attribute__((unused))*argv[])
+int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%d\n", argc - 1);
 	return 0;
 }
