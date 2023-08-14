3d018db899d83e9175e041412cdb97c38b2b70fa
roteque <95283959+roteque@users.noreply.github.com>
2023-08-15 00:16:18 +0100

    Task 2 done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * Main - A program that prints the name of the file it was compile from
- * followed by a new line
- *
- */
 
-int main()
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
+
