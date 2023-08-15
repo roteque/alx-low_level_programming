ca6a1b25995e63b5c2c44adcdb3b9c582a9d9dc1
roteque <95283959+roteque@users.noreply.github.com>
2023-08-15 19:09:10 +0100

    Task 2 completed and executed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "(Nothing)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "(Nothing)";
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
