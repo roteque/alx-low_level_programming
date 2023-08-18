df77dbbe8d164d9aefa2ef40e292043ee964d259
roteque <95283959+roteque@users.noreply.github.com>
2023-08-18 02:10:40 +0100

    Task 1 completed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,12 +6,23 @@
  * main - A program to print numbers followeed by a new line.
  * @ separator: the string to be printed between two numbers.
  * @ n: the number of integers passed to the function.
- * 
  * NULL: if separator is NULL, don't print
  *
+ * Return Always 0 (Success)
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {	
-	va_list print
+	va_list print;
+		unsigned int i;
+
+		va_start(print, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d\n", va_arg(print, int));
+			if (i != (n-1) && separator != NULL)
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(print);
 }
