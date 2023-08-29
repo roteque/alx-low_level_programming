c9cb60979f3a26f98685e7417b14c5f2c0711082
Oluwasegun_Omogoye <olusomogoye@gmail.com>
2023-08-29 22:01:16 +0000

    Task 9 done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,13 +11,17 @@
  *
  * Return: pointer to the new node, or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+
 	unsigned int i;
+
 	listint_t *new;
 	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
+
 	if (!new || !head)
 	return (NULL);
 
@@ -28,18 +32,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 	new->next = *head;
 	*head = new;
+
 	return (new);
 	}
-
 	for (i = 0; temp && i < idx; i++)
 	{
 	if (i == idx - 1)
 	{
 	new->next = temp->next;
 	temp->next = new;
+
 	return (new);
 	}
 	else
 	temp = temp->next;
 	}
+	return (NULL);
 }
