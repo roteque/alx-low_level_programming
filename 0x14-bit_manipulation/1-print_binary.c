d57db05212b62eb8a984d70601d820f48093b969
Oluwasegun_Omogoye <olusomogoye@gmail.com>
2023-09-01 08:46:59 +0000

    putchar file created

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -467,26 +467,34 @@ unsigned int binary_to_uint(const char *b)
  * @n: number to print in binary
 
  */
+#include "main.h"
+
+
+/**
+
+ * print_binary - prints the binary equivalent of a decimal number
+
+ * @n: number to print in binary
+
+ */
 
 void print_binary(unsigned long int n)
-
 {
 	int i, count = 0;
 	unsigned long int current;
 
-	for (i = 63; i >= 0; i--)		
+	for (i = 63; i >= 0; i--)
 	{
 		current = n >> i;
 
-                if (current & 1)
-
+		if (current & 1)
+		{
 		_putchar('1');
 		count++;
-    }
+		}
 		else if (count)
 		_putchar('0');
-	}
+		}
 	if (!count)
 	_putchar('0');
-
 }
