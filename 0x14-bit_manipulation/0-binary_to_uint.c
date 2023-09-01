564599a2f02df85f5bd14d54b59b3a41458f2947
Oluwasegun_Omogoye <olusomogoye@gmail.com>
2023-09-01 00:56:11 +0000

    Still on task 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 {
 	unsigned int decimal_value = 0;
-	
+
 	if (b == NULL)
 {
 	return (0);
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,492 @@
-void print_binary(unsigned long int n) {
-    if (n == 0) {
-        printf("0b0");
-        return;
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
-    int num_bits = sizeof(unsigned long int) * 8;
-    int leading_zeroes = 1;
+/**
+ * print_binary - prints the binary of any number
+ * @n: number to print in binary
+ *
+ * Return: 0
+ */
 
-    unsigned long int mask = 1UL << (num_bits - 1);
-    
-    for (int i = 0; i < num_bits; i++) {
-        if (leading_zeroes && (n & mask) == 0) {
-		mask >>= 1;
-            continue;
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+0x14. C - Bit Manipulation
+0x14. C - Bit Manipulation
+
+
+README.md
+
+
+Project
+
+0x14. C - Bit Manipulation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main.h
+
+
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+unsigned int binary_to_uint(const char *b);
+
+void print_binary(unsigned long int n);
+
+int get_bit(unsigned long int n, unsigned int index);
+
+int set_bit(unsigned long int *n, unsigned int index);
+
+int clear_bit(unsigned long int *n, unsigned int index);
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int _atoi(const char *s);
+
+int _putchar(char c);
+
+int get_endianness(void);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_putchar.c
+
+
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+
+ * _putchar - writes the character c to stdout
+
+ * @c: The character to print
+
+ *
+
+ * Return: On success 1.
+
+ * On error, -1 is returned, and errno is set appropriately.
+
+ */
+
+int _putchar(char c)
+
+{
+
+        return (write(1, &c, 1));
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+0-binary_to_uint.c
+
+
+#include "main.h"
+
+
+/**
+
+ * binary_to_uint - converts a binary number to unsigned int
+
+ * @b: string containing the binary number
+
+ *
+
+ * Return: the converted number
+
+ */
+
+unsigned int binary_to_uint(const char *b)
+
+{
+
+        int i;
+
+        unsigned int dec_val = 0;
+
+
+        if (!b)
+
+                return (0);
+
+
+        for (i = 0; b[i]; i++)
+
+        {
+
+                if (b[i] < '0' || b[i] > '1')
+
+                        return (0);
+
+                dec_val = 2 * dec_val + (b[i] - '0');
+
         }
-        leading_zeroes = 0;
-        printf("%d", (n & mask) != 0);
-        mask >>= 1;
+
+
+        return (dec_val);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1-print_binary.c
+
+
+#include "main.h"
+
+
+/**
+
+ * print_binary - prints the binary equivalent of a decimal number
+
+ * @n: number to print in binary
+
+ */
+0x14. C - Bit Manipulation
+0x14. C - Bit Manipulation
+
+
+README.md
+
+
+Project
+
+0x14. C - Bit Manipulation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main.h
+
+
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+unsigned int binary_to_uint(const char *b);
+
+void print_binary(unsigned long int n);
+
+int get_bit(unsigned long int n, unsigned int index);
+
+int set_bit(unsigned long int *n, unsigned int index);
+
+int clear_bit(unsigned long int *n, unsigned int index);
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int _atoi(const char *s);
+
+int _putchar(char c);
+
+int get_endianness(void);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_putchar.c
+
+
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+
+ * _putchar - writes the character c to stdout
+
+ * @c: The character to print
+
+ *
+
+ * Return: On success 1.
+
+ * On error, -1 is returned, and errno is set appropriately.
+
+ */
+
+int _putchar(char c)
+
+{
+
+        return (write(1, &c, 1));
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+0-binary_to_uint.c
+
+
+#include "main.h"
+
+
+/**
+
+ * binary_to_uint - converts a binary number to unsigned int
+
+ * @b: string containing the binary number
+
+ *
+
+ * Return: the converted number
+
+ */
+
+unsigned int binary_to_uint(const char *b)
+
+{
+
+        int i;
+
+        unsigned int dec_val = 0;
+
+
+        if (!b)
+
+                return (0);
+
+
+        for (i = 0; b[i]; i++)
+
+        {
+
+                if (b[i] < '0' || b[i] > '1')
+
+                        return (0);
+
+                dec_val = 2 * dec_val + (b[i] - '0');
+
+        }
+
+
+        return (dec_val);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1-print_binary.c
+
+
+#include "main.h"
+
+
+/**
+
+ * print_binary - prints the binary equivalent of a decimal number
+
+ * @n: number to print in binary
+
+ */
+
+void print_binary(unsigned long int n)
+
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)		
+	{
+		current = n >> i;
+
+                if (current & 1)
+
+		_putchar('1');
+		count++;
     }
+		else if (count)
+		_putchar('0');
+	}
+	if (!count)
+	_putchar('0');
+
 }
